d479c9e9abd9e5ec4a99155bbd8b19fc122592bd
Chris Joel <chrisjoel68157@gmail.com>
2025-10-19 13:45:22 -0300

    Expose Node, Component, and Circuit classes to Python

--- bindings/bindings.cpp
+++ bindings/bindings.cpp
@@ -1,7 +1,33 @@
 #include <pybind11/pybind11.h>
+#include <pybind11/stl.h>
+#include "../include/Node.h"
+#include "../include/Component.h"
+#include "../include/Circuit.h"
 
 namespace py = pybind11;
 
 PYBIND11_MODULE(mesh_solver, m) {
     m.doc() = "Mesh Solver Python Bindings";
+
+    py::class_<Node>(m, "Node")
+        .def(py::init<int>())
+        .def("getId", &Node::getId)
+        .def("getVoltage", &Node::getVoltage)
+        .def("setVoltage", &Node::setVoltage)
+        .def("isFixed", &Node::isFixed)
+        .def("setFixed", &Node::setFixed);
+
+    py::class_<Component>(m, "Component");
+
+    py::class_<Resistor, Component>(m, "Resistor")
+        .def(py::init<double, Node*, Node*>())
+        .def("getResistance", &Resistor::getResistance)
+        .def("getCurrent", &Resistor::getCurrent);
+
+    py::class_<Circuit>(m, "Circuit")
+        .def(py::init<>())
+        .def("addNode", &Circuit::addNode)
+        .def("addComponent", &Circuit::addComponent)
+        .def("getNodes", &Circuit::getNodes)
+        .def("getComponents", &Circuit::getComponents);
 }
